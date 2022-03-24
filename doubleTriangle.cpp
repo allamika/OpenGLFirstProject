11a6919b4b08de0e50b27ff82caa170e4fa22702
MAllanos <mikael.allanos@gmail.com>
2022-03-24 16:48:28 +0100

    shaders in out

--- doubleTriangle.cpp
+++ doubleTriangle.cpp
@@ -7,20 +7,17 @@
 #include <GLFW/glfw3.h>
 
 const char *vertexShaderSource = "#version 330 core\n"
-		"layout (location = 0) in vec3 aPos; // the position variable has attribute position 0\n"
-		"out vec4 vertexColor; // specify a color output to the fragment shader\n"
+		"layout (location = 0) in vec3 aPos;\n"
 		"void main()\n"
 		"{\n"
-		"    gl_Position = vec4(aPos, 1.0); // see how we directly give a vec3 to vec4's constructor\n"
-		"    vertexColor = vec4(0.5, 0.0, 0.0, 1.0); // set the output variable to a dark-red color\n"
-		"}\n";//source vertex shader
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";//source vertex shader
 		
 const char *fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-		"in vec4 vertexColor; // the input variable from the vertex shader (same name and same type)\n"
+		"out vec4 FragColor;\n"
 		"void main()\n"
 		"{\n"
-		"    FragColor = vertexColor;\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 		"}\n";//fragment shader source code
 
 
@@ -167,7 +164,8 @@ int main()
 	
 	
 	
-		
+	
+	
 	//generate pipeline
 	unsigned int VAO, EBO;
 	unsigned int shaderProgram;	
